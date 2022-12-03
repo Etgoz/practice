e6d177768d4bb7d144d4a817f7ba174d14c15d1b
etgoz <etgoz151@gmail.com>
2022-12-03 16:12:33 +0200

    hebDate done

--- src/scripts/js/hebDate.js
+++ src/scripts/js/hebDate.js
@@ -1,1 +1,17 @@
-//using get method, send the date the user chose and translate it to hebrew date "https://www.hebcal.com/converter?cfg=json&date=2011-06-02&g2h=1&strict=1"
+document.addEventListener("DOMContentLoaded", async (event) => {
+    const params = new URLSearchParams(document.location.search);
+    const userDate = params.get("userDate");
+    try {
+        const response = await fetch(`https://www.hebcal.com/converter?cfg=json&date=${userDate}&g2h=1&strict=1`);
+        const hebDate = await response.json();
+        console.log(hebDate.hebrew);
+        const inputDate = document.getElementById("inputDate");
+        const userDateParts = userDate.split("-");
+        inputDate.innerHTML = `${userDateParts[2]}/${userDateParts[1]}/${userDateParts[0]} →`;
+        const hebrewPlace = document.getElementById("hebrewDate");
+        hebrewPlace.innerText = hebDate.hebrew;
+    }
+    catch (e) {
+        console.log(e);
+    }
+});
--- src/scripts/ts/hebDate.ts
+++ src/scripts/ts/hebDate.ts
@@ -1,1 +1,31 @@
 //using get method, send the date the user chose and translate it to hebrew date "https://www.hebcal.com/converter?cfg=json&date=2011-06-02&g2h=1&strict=1"
+interface HebrewDate {
+	heDateParts: {
+		y: string;
+		m: string;
+		d: string;
+	};
+	hebrew: string;
+}
+
+document.addEventListener("DOMContentLoaded", async (event) => {
+	const params: URLSearchParams = new URLSearchParams(document.location.search);
+	const userDate: string = params.get("userDate");
+
+	try {
+		const response = await fetch(
+			`https://www.hebcal.com/converter?cfg=json&date=${userDate}&g2h=1&strict=1`
+		);
+		const hebDate: HebrewDate = await response.json();
+		console.log(hebDate.hebrew);
+
+		const inputDate = document.getElementById("inputDate");
+		const userDateParts = userDate.split("-");
+		inputDate.innerHTML = `${userDateParts[2]}/${userDateParts[1]}/${userDateParts[0]} →`;
+
+		const hebrewPlace = document.getElementById("hebrewDate");
+		hebrewPlace.innerText = hebDate.hebrew;
+	} catch (e) {
+		console.log(e);
+	}
+});
